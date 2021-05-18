73b70518c68976a9f9b4672ca0d5104fe5673f26
Francois Kritzinger <francois@codesynthesis.com>
2021-05-18 16:47:19 +0200

    Generate packages for levels 2-4 libraries

---
+++ downstream/libs/timer/test/basics/driver.cpp
@@ -0,0 +1,10 @@
+// @@ This custom smoke test might only be temporary. See the comments in
+//    `generate` for why this is necessary.
+//
+#include <boost/timer/timer.hpp>
+
+int
+main ()
+{
+  return 0;
+}
