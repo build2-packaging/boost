250eda82cef650b6e2276f8cd3d5868a709a7399
Francois Kritzinger <francois@codesynthesis.com>
2021-08-26 11:58:50 +0200

    Generated changes

--- libboost-fiber/tests/basics/driver-numa.cpp
+++ libboost-fiber/tests/basics/driver-numa.cpp
@@ -1,10 +1,27 @@
+#include <boost/fiber/exceptions.hpp> // fiber_error
 #include <boost/fiber/numa/topology.hpp>
 
-using namespace std;
+#include <system_error> // errc
 
 int
 main ()
 {
-  static_cast<void> (boost::fibers::numa::topology ());
+  // Note that on some platforms (currently Mac OS and MinGW), topology() is
+  // not implemented and throws fiber_error exception with the
+  // function_not_supported code. There, we make sure that if it fails, then
+  // it happens exactly by this reason, in case a real (generic)
+  // implementation (which may throw other kinds of exceptions) is added to
+  // src/numa/topology.cpp upstream.
+  //
+  try
+  {
+    static_cast<void> (boost::fibers::numa::topology ());
+  }
+  catch (const boost::fibers::fiber_error& e)
+  {
+    if (e.code () != std::errc::function_not_supported)
+      throw;
+  }
+
   return 0;
 }
