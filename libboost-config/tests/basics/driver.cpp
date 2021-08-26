d42cdecc9d8e9b593eb65b2aefb7933b6bda85b8
Francois Kritzinger <francois@codesynthesis.com>
2021-08-26 11:58:50 +0200

    Generated changes

--- libboost-config/tests/basics/driver.cpp
+++ libboost-config/tests/basics/driver.cpp
@@ -1,5 +1,13 @@
 #include <boost/config.hpp>
 
+// Some of the Boost.Config macros fail to detect certain C++11 features under
+// libstdc++ on Mac OS (our gcc/homebrew Mac OS configs) so flush out any
+// other platforms for which detection is broken.
+//
+#ifdef BOOST_NO_CXX11_HDR_MUTEX
+#error "BOOST_NO_CXX11_HDR_MUTEX is defined"
+#endif
+
 int
 main ()
 {
